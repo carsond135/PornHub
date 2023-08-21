4f7cb7b3043a8c770f154f9405624f444df12af0
Carson B Dunlap <cbd135.dev@gmail.com>
2023-08-20 20:47:35 -0400

    final commit

---
+++ PornHub/config.py
@@ -0,0 +1,10 @@
+from typing import List
+
+API_ID: int = insert_your_own_api_id_here
+API_HASH: str = "insert_your_own_api_hash_here"
+TOKEN: str = "insert_your_bot_token_here"
+
+log_chat: int = -1001662591986
+sub_chat: str = "SakuraEmpireTeam"
+sudoers: List[int] = [2137482758, 1249591948]
+prefixs: List[str] = ["/", "!", ".", "$", "-"]
