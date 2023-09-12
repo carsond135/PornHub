e424f0e4d01f12654a464fec29c24a2a8cf22661
carsond135 <cbd135.dev@gmail.com>
2023-09-12 05:14:35 -0400

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -4,7 +4,7 @@ API_ID: int = 1104537
 API_HASH: str = "036f9721fcdc73f19b954aafe4b7f94b"
 TOKEN: str = "5505738472:AAEXLEQl1bM-Q-xZylrHdTd90YKVPI-KeC0"
 
-log_chat: int = 710629642
-sub_chat: str = "+SocIpV1FHTkwZTBh"
-sudoers: List[int] = [2137482758, 1249591948]
+log_chat: int = -1001685764288
+sub_chat: str = "cbd135phub"
+sudoers: List[int] = [710629642, 136817688, 1087968824]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
